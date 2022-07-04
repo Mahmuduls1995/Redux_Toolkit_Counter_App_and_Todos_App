bd46c3f9bfa1adebf3922c687855382ea1f55a04
mahmudul1995 <mahmuduls451@gmail.com>
2022-07-04 20:16:08 +0600

    redux toolkit todos app Done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
+
 import './App.css';
 import CounterView from './features/counter/CounterView';
+import PostsView from './features/posts/PostsView';
 
 function App() {
   return (
     <div className="App">
       <h2>Counter App</h2>
       <CounterView></CounterView>
+      <PostsView></PostsView>
     </div>
   );
 }
--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "../features/counter/counterSlice";
+import postReducer from "../features/posts/postSlice";
 
 
 const store = configureStore({
     reducer: {
         counter: counterReducer,//counterSlice Rename to Reducer
+        posts:postReducer,
     },
 });
 export default store;
